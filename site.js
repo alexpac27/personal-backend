0dcfabdaf1de293feafb47a7c628378cd57595e8
alexpac27 <alexpac27@gmail.com>
2020-12-21 23:09:17 -0500

    clear section content when hitting nav bar selection

--- site.js
+++ site.js
@@ -19,11 +19,18 @@ document.addEventListener("DOMContentLoaded", () => {
         
         if (e.target.className === "home"){
             console.log("clicked home")
+            contentDiv.innerHTML = `
+            <h1 class="title">Alexandra Ortiz</h1>
+            <p>hello, my name is Alex.</p>
+            <p>I am a web developer pursuing a passion to build beutiful things.</p>
+            `
         }if (e.target.className === "projects"){
             console.log("clicked projects")
+            contentDiv.innerHTML = ''
 
         }if (e.target.className === "contact"){
             console.log("clicked contact")
+            contentDiv.innerHTML = ''
         }
     })
 })
