5167a2946db02f0aadf8db4c7d92f6e2f28d1fe5
alexpac27 <alexpac27@gmail.com>
2020-12-21 22:39:17 -0500

    event listener for nav

--- site.js
+++ site.js
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const navBar = document.createElement('div')
     navBar.classList += "navBar"
     navBar.innerHTML = `
-        <div>Home</div>
-        <div>Projects</div>
-        <div>Contact</div>
+        <div class="home">Home</div>
+        <div class="projects">Projects</div>
+        <div class="contact">Contact</div>
     `
     const body = document.querySelector('body')
-    
     body.prepend(navBar)
+
+    const titleDiv = document.createElement('div')
+    titleDiv.className = "title"
+
+    navBar.addEventListener("click", (e)=>{
+        console.log("click", e.target)
+    })
 })
 
