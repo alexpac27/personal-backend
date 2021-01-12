40fa7250c62d6d302f641e99f7006cb164c2a1a1
alexpac27 <alexpac27@gmail.com>
2021-01-11 23:31:22 -0500

    transition threshold corrected

--- site.js
+++ site.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
    
+const intro = document.querySelector('.intro-section')
+const body = document.querySelector('body')
 
-  
+
+const introOptions = {
+    threshold: .90,
+    rootMargin: "40px 0px 0px 0px"
+};
+
+const introObserver = new IntersectionObserver(function(entires, introObserver){
+    entires.forEach( entry => {
+        if( entry.isIntersecting) {
+            body.classList.add("intro-scrolled")
+        } else {
+            body.classList.remove("intro-scrolled")
+        }
+    } )
+}, introOptions); 
+
+introObserver.observe(intro)
 })
 
